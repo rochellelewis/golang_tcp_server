242218c49b9f3158c0d19d3c1aeea548e957389d
Rochelle Lewis <rochelle.lewis@logrhythm.com>
2022-05-24 12:08:38 -0600

    add simple tcp server code

---
+++ tcpserver.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+//See: https://www.youtube.com/watch?v=l3KCOjhFXbQ
+//Test using PuTTY telnet connection to listening port
+
+func main() {
+	//Listen
+	//Accept Connection
+	//Handle Connection -> Thread
+
+	dstream, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer dstream.Close()
+
+	for {
+		conn, err := dstream.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		//go routine
+		go handle(conn)
+	}
+
+}
+
+func handle(conn net.Conn) {
+	for {
+		data, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(data)
+	}
+	conn.Close()
+}
